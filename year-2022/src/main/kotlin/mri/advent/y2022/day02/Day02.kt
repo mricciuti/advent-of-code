ba3c31ffb61efcf9fbe22baa6f5034bde1760c16
mricciuti <matt74000@yahoo.fr>
2022-12-02 13:56:46 +0100

    day02

--- year-2022/src/main/kotlin/mri/advent/y2022/day02/Day02.kt
+++ year-2022/src/main/kotlin/mri/advent/y2022/day02/Day02.kt
@@ -3,14 +3,53 @@ package mri.advent.y2022.day02
 import mri.advent.y2022.BaseDay
 
 /** --- Day 02:  --- */
-class Day02(inFile: String = "/day02.in"): BaseDay(inFile)  {
+class Day02(inFile: String = "/day02.in") : BaseDay(inFile) {
 
-    override fun part2(): Any {
-        return ""
+    enum class RoundOutcome(val score: Int) { LOST(0), DRAW(3), WON(6) }
+
+    enum class Shape(val score: Int) {
+        ROCK(1), PAPER(2), SCISSORS(3);
+
+        fun versus(other: Shape) =
+            if (this == other) RoundOutcome.DRAW
+            else if (this == ROCK && other == SCISSORS || this == PAPER && other == ROCK || this == SCISSORS && other == PAPER) RoundOutcome.WON
+            else RoundOutcome.LOST
     }
 
+    private fun parseOutcome(asChar: Char) = RoundOutcome.values()[asChar - 'X']
+    private fun parseShape(shapeChar: Char) = Shape.values()[if (shapeChar >= 'X') shapeChar - 'X' else shapeChar - 'A']
+
+    data class Round(val myHand: Shape, val otherHand: Shape) {
+        private val outcome = myHand.versus(otherHand)
+        fun score() = myHand.score + outcome.score
+    }
+
+    // part 2 -------------------------
+    private fun decideShape(otherHand: Shape, expectedOutput: RoundOutcome) = Shape.values().first { it.versus(otherHand) == expectedOutput }
+
+    override fun part2(): Any {
+
+        // input:  list (  opponentShape -> expected outcome )
+        val guide = data.lines()
+            .map { it.split(" ") }
+            .map { parseShape(it[0].first()) to parseOutcome(it[1].first()) }
+
+        return guide.map {
+            Round(otherHand = it.first, myHand = decideShape(it.first, it.second))
+        }.sumOf { it.score() }
+    }
+
+    // part 1 -------------------------
     override fun part1(): Any {
-        return ""
+
+        // input:  list ( opponentShape -> myShape )
+        val guide = data.lines()
+            .map { it.split(" ") }
+            .map { parseShape(it[0].first()) to parseShape(it[1].first()) }
+
+        return guide
+            .map { Round(otherHand = it.first, myHand = it.second) }
+            .sumOf { it.score() }
     }
 }
 
--- year-2022/src/test/kotlin/mri/advent/y2022/day02/Day02Test.kt
+++ year-2022/src/test/kotlin/mri/advent/y2022/day02/Day02Test.kt
@@ -2,6 +2,8 @@ package mri.advent.y2022.day02
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import mri.advent.y2022.day02.Day02.Shape.*
+import mri.advent.y2022.day02.Day02.RoundOutcome.*
 
 /**
  * --- Day 02 Test:  ---
@@ -12,11 +14,24 @@ class Day02Test {
 
     @Test
     fun `test part1`() {
-        assertEquals(7, Day02(sample).part1())
+        assertEquals(15, Day02(sample).part1())
     }
 
     @Test
     fun `test part2`() {
-        assertEquals(5, Day02(sample).part2())
+        assertEquals(12, Day02(sample).part2())
+    }
+
+    @Test
+    fun `test shape versus`() {
+        val rules = listOf(
+            (ROCK to ROCK) to DRAW, (ROCK to PAPER) to LOST, (ROCK to SCISSORS) to WON,
+            (PAPER to ROCK) to WON, (PAPER to PAPER) to DRAW, (PAPER to SCISSORS) to LOST,
+            (SCISSORS to ROCK) to LOST, (SCISSORS to PAPER) to WON, (SCISSORS to SCISSORS) to DRAW,
+        )
+        rules.forEach {
+            assertEquals(it.second, it.first.first.versus(it.first.second))
+        }
+
     }
 }
